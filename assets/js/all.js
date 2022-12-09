89bd5dee3cc88d0c85f7a9ff7ecd2fd06cfeebf0
tanukili <108317269+tanukili@users.noreply.github.com>
2022-12-09 19:05:53 +0800

    Update 2022-12-09T11:05:51.680Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,66 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
+document.addEventListener('DOMContentLoaded', function () {
+  var ele = document.querySelector('.recommendation-wall');
+  ele.style.cursor = 'grab';
+  var pos = {
+    top: 0,
+    left: 0,
+    x: 0,
+    y: 0
+  };
+
+  var mouseDownHandler = function mouseDownHandler(e) {
+    ele.style.cursor = 'grabbing';
+    ele.style.userSelect = 'none';
+    pos = {
+      left: ele.scrollLeft,
+      top: ele.scrollTop,
+      // Get the current mouse position
+      x: e.clientX,
+      y: e.clientY
+    };
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+  };
+
+  var mouseMoveHandler = function mouseMoveHandler(e) {
+    // How far the mouse has been moved
+    var dx = e.clientX - pos.x;
+    var dy = e.clientY - pos.y; // Scroll the element
+
+    ele.scrollTop = pos.top - dy;
+    ele.scrollLeft = pos.left - dx;
+  };
+
+  var mouseUpHandler = function mouseUpHandler() {
+    ele.style.cursor = 'grab';
+    ele.style.removeProperty('user-select');
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+  }; // Attach the handler
+
+
+  ele.addEventListener('mousedown', mouseDownHandler);
+}); // menu 切換
+
+var menuOpenBtn = document.querySelector('.menuToggle');
+var linkBtn = document.querySelectorAll('.topBar-menu a');
+var menu = document.querySelector('.topBar-menu');
+menuOpenBtn.addEventListener('click', menuToggle);
+linkBtn.forEach(function (item) {
+  item.addEventListener('click', closeMenu);
 });
+
+function menuToggle() {
+  if (menu.classList.contains('openMenu')) {
+    menu.classList.remove('openMenu');
+  } else {
+    menu.classList.add('openMenu');
+  }
+}
+
+function closeMenu() {
+  menu.classList.remove('openMenu');
+}
 //# sourceMappingURL=all.js.map
